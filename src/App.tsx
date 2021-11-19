bf35878c2cec70118bd807345046925ebbf2169c
Arthur Brandão <arthurbrdantas@gmail.com>
2021-11-19 03:11:58 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
 
-function App() {
+import { Box, Center, Flex, Text, Input, Spacer, Button } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+
+import MovieItem from './components/movieItem/movieItem'
+import NoMovieComponent from './components/noMovieComponent/noMovieComponent'
+import { Movie } from './models/movie'
+
+const App: React.FC = () => {
+  const [value, setValue] = useState('')
+  const [results, setResults] = useState([])
+
+  async function searchMovies(query: string) {
+    try {
+      const { data } = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=0f282ded95aff7bb89651d650fc76916&language=pt-BR&query=${query}&page=1&include_adult=false`)
+      console.log(data.results)
+      setResults(data.results)
+    }catch(error) {
+      console.log(error)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Center h='100vh' bgGradient="linear(to-r, blue.900, cyan.900)" p={10}>
+      <Flex direction='column' align='center' maxWidth='800px' minWidth='250px' w='80%' maxHeight='80vh'>
+        <Box bgColor='white' borderRadius={4} boxShadow='xl' mb={10} p={6} w='100%'>
+          <Text fontWeight='bold' fontSize='2xl' mb={2}>Listagem de filmes</Text>
+          <Flex>
+            <Input autoFocus focusBorderColor='blue.900' placeholder='Procure por um filme...' variant='flushed' mb={4} onChange={(e) => setValue(e.target.value)} value={value}/>
+            <Spacer />
+            <Button ml={4} bgColor='blue.900' color='white' leftIcon={<SearchIcon />} px={8} onClick={() => searchMovies(value)}>Pesquisar</Button>
+          </Flex>
+        </Box>
+        
+        <Box bgColor='white' p={6} borderRadius={4} overflowY='auto' boxShadow='xl' w='100%'>
+          {results.length > 0?(results.map((result: Movie) => <MovieItem movie={result} />)):<NoMovieComponent />}
+        </Box>
+      </Flex>
+    </Center>
   );
 }
 
---
+++ src/components/viewMovieDetails/viewMovieDetails.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  UseDisclosureReturn,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Button,
+  ModalFooter,
+  Flex,
+} from "@chakra-ui/react"
+
+import { Movie } from '../../models/movie'
+
+type Props = {
+  movie: Movie
+  disclosure: UseDisclosureReturn
+}
+
+const ViewMovieDetails: React.FC<Props> = ({ movie, disclosure }) => {
+
+  return (
+    <Modal isCentered isOpen={disclosure.isOpen} onClose={disclosure.onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{movie.title}</ModalHeader>
+        <ModalBody mb={2}>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Informação</Th>
+                <Th>Valor</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Adulto</Td>
+                <Td>{movie.adult ? 'Sim' : 'Não'}</Td>
+              </Tr>
+              <Tr>
+                <Td>Idioma original</Td>
+                <Td>{movie.original_language}</Td>
+              </Tr>
+              <Tr>
+                <Td>Título original</Td>
+                <Td>{movie.original_title}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </ModalBody>
+        <ModalFooter>
+          <Flex justifyContent='center'>
+            <Button backgroundColor='blue.900' color='white' onClick={disclosure.onClose}>
+              Fechar
+            </Button>
+          </Flex>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default ViewMovieDetails
